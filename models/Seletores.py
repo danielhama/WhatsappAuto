c5e23d68da6bbdf1f85f22dac139b4d457d99b95
Daniel Hamamura <DanielHamamura@hotmail.com>
2023-06-09 12:57:59 -0300

    Algumas alterações

--- models/Seletores.py
+++ models/Seletores.py
@@ -1,5 +1,6 @@
 campo_msg = "._3Uu1_"
-campo_msg2 = ".to2l77zo.gfz4du6o.ag5g9lrv.bze30y65.kao4egtt"
+campo_msg1 = "lhggkp7q.qq0sjtgm.jxacihee.c3x5l3r8.b9fczbqn.t35qvd06.m62443ks.rkxvyd19.c5h0bzs2.bze30y65.kao4egtt"
+campo_msg2 = "#main > footer > div._2lSWV._3cjY2.copyable-area > div > span:nth-child(2) > div > div._1VZX7 > div._3Uu1_ > div > div"
 ok = "#app > div > span:nth-child(2) > div > span > div > div > div > div > div > div.p357zi0d.ns59xd2u.kcgo1i74.gq7nj7y3.lnjlmjd6.przvwfww.mc6o24uu.e65innqk.le5p0ye3 > div > button > div > div"
 # ok = "#app > div > span:nth-child(2) > div > span > div > div > div > div > div > div.p357zi0d.ns59xd2u.kcgo1i74.gq7nj7y3.lnjlmjd6.przvwfww.mc6o24uu.e65innqk.le5p0ye3 > div > div" #._20C5O"
 #app > div > span:nth-child(2) > div > span > div > div > div > div > div > div.p357zi0d.ns59xd2u.kcgo1i74.gq7nj7y3.lnjlmjd6.przvwfww.mc6o24uu.e65innqk.le5p0ye3 > div > div
@@ -20,7 +21,8 @@ nome_CSS = "#pane-side > div:nth-child(1) > div > div > div:nth-child(4) > div >
 pesquisa_contato = '/html/body/div[1]/div[1]/div[1]/div[3]/div/div[1]/div/label/div/div[2]'
 ultima_msg = "#main > div._2gzeB > div > div._33LGR > div._3K4-L > div:nth-child(30) > div > div.ItfyB._3nbHh > div._22Msk > div.copyable-text > div > span.i0jNr.selectable-text.copyable-text"
 #main > div._2gzeB > div > div._33LGR > div._3K4-L > div:nth-child(8) > div > div.ItfyB._3nbHh > div._22Msk > div.copyable-text > div._1Gy50 > span.i0jNr.selectable-text.copyable-text
-barra_superior = '#main > header > div._24-Ff > div > div > span'
+barra_superior = '._3W2ap'
 #main ._2gzeB ._33LGR ._3K4-L .ItfyB._3nbHh ._22Msk div.copyable-text ._1Gy50 span.i0jNr.selectable-text.copyable-text span
 "._2gzeB ._33LGR ._3K4-L .ItfyB._3nbHh ._22Msk > div.copyable-text > div._1Gy50 > span.i0jNr.selectable-text.copyable-text"
 bloqueado = "._8AbMF"
+check = "#main > div._2gzeB > div > div._5kRIK > div.n5hs2j7m.oq31bsqd.gx1rr48f.qh5tioqs > div:nth-child(2) > div > div > div > div.ItfyB._3nbHh > div.cm280p3y.to2l77zo.n1yiu2zv.c6f98ldp.ooty25bp.oq31bsqd > div.g0rxnol2.g2bpp9au.faxx4fbg.aja0i6dq.jnwc1y2a.bn7x0pqn.qnz2jpws > div > div > span"
